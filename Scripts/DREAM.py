bca54d65eaba5f69b41bed59c2ae158ed6178d01
angryfarmer <ericyu922@gmail.com>
2017-08-02 20:08:42 -0700

    Stochastic Gradient Descent added

--- Scripts/DREAM.py
+++ Scripts/DREAM.py
@@ -12,7 +12,7 @@ number_of_product_features 	= in_dims[3]
 number_of_time_steps 		= in_dims[1]
 number_of_users 			= in_dims[0]
 
-print(in_dims)
+# print(in_dims)
 ## Ranks for Each Layer
 L_1_nodes 		= number_of_product_features
 user_features 	= number_of_product_features
@@ -34,47 +34,56 @@ product_weights = tf.Variable(tf.truncated_normal([number_of_products,number_of_
 
 ## Placeholders to run graph
 input_data					= tf.placeholder(tf.float32,[None,number_of_time_steps,number_of_products,number_of_product_features])
-prev_cell_state 			= tf.zeros([number_of_users,L_1_nodes])
-prev_hidden_layer 			= tf.zeros([number_of_users,L_1_nodes])
+prev_cell_state 			= tf.zeros([tf.shape(input_data)[0],L_1_nodes])
+prev_hidden_layer 			= tf.zeros([tf.shape(input_data)[0],L_1_nodes])
 cost 						= tf.reduce_sum(tf.zeros([1]))
 user_basket_representation 	= tf.zeros([tf.shape(input_data)[0],number_of_product_features])
 
 for n in range(number_of_time_steps - 1):
-	## Prepare input using max of each feature over last n baskets
-	user_baskets_0_to_n 		= tf.slice(input_data,[0,n,0,0],[-1,1,-1,-1])
-	user_baskets_0_to_n 		= tf.multiply(user_baskets_0_to_n,product_weights)
-	user_baskets_0_to_n 		= tf.reduce_max(user_baskets_0_to_n,2)
-	user_baskets_0_to_n			= tf.concat([user_baskets_0_to_n,tf.expand_dims(user_basket_representation,1)],1)
-	user_basket_representation 	= tf.reduce_max(user_baskets_0_to_n,1)
-	
-	## Basic LSTM Cell setup
-	forget_gate 				= tf.exp(tf.add(tf.matmul(user_basket_representation,W_1_f),tf.matmul(prev_hidden_layer,W_h_f)))
-	input_gate 					= tf.exp(tf.add(tf.matmul(user_basket_representation,W_1_i),tf.matmul(prev_hidden_layer,W_1_i)))
-	candidate_value 			= tf.tanh(tf.add(tf.matmul(user_basket_representation,W_1_c),tf.matmul(prev_hidden_layer,W_h_c)))
-	new_cell_state 				= tf.add(tf.multiply(input_gate,candidate_value),tf.multiply(forget_gate,prev_cell_state))
-	
-	## Generated User Preference
-	L_1_output 					= tf.exp(tf.add(tf.matmul(user_basket_representation,W_1_o),tf.matmul(prev_hidden_layer,W_h_o)))
-	user_preference 			= tf.exp(tf.matmul(L_1_output,W_2))
-	## Save Cell States and Hidden Layer for next Time Step
-	prev_hidden_layer 			= tf.multiply(L_1_output,tf.tanh(new_cell_state))
-	prev_cell_state 			= tf.identity(new_cell_state)
+	with tf.name_scope("Time_Step_{}".format(n)):
+		## Prepare input using max of each feature over last n baskets
+		with tf.name_scope("Basket_Representation_{}".format(n)):
+			user_baskets_0_to_n 		= tf.slice(input_data,[0,n,0,0],[-1,1,-1,-1])
+			user_baskets_0_to_n 		= tf.multiply(user_baskets_0_to_n,product_weights)
+			user_baskets_0_to_n 		= tf.reduce_max(user_baskets_0_to_n,2)
+			user_baskets_0_to_n			= tf.concat([user_baskets_0_to_n,tf.expand_dims(user_basket_representation,1)],1)
+			user_basket_representation 	= tf.reduce_max(user_baskets_0_to_n,1)
+		
+		## Basic LSTM Cell setup
+		with tf.name_scope("Cell_Gates_{}".format(n)):
+			forget_gate 				= tf.sigmoid(tf.add(tf.matmul(user_basket_representation,W_1_f),tf.matmul(prev_hidden_layer,W_h_f)))
+			input_gate 					= tf.sigmoid(tf.add(tf.matmul(user_basket_representation,W_1_i),tf.matmul(prev_hidden_layer,W_h_i)))
+			candidate_value 			= tf.tanh(tf.add(tf.matmul(user_basket_representation,W_1_c),tf.matmul(prev_hidden_layer,W_h_c)))
+			new_cell_state 				= tf.add(tf.multiply(input_gate,candidate_value),tf.multiply(forget_gate,prev_cell_state))
+		
+		## Generated User Preference
+		with tf.name_scope("Cell_Outputs_{}".format(n)):
+			L_1_output 					= tf.sigmoid(tf.add(tf.matmul(user_basket_representation,W_1_o),tf.matmul(prev_hidden_layer,W_h_o)))
+			user_preference 			= tf.sigmoid(tf.matmul(L_1_output,W_2))
+		## Save Cell States and Hidden Layer for next Time Step
+		with tf.name_scope("Hidden_Layers_And_Cell_States_{}".format(n)):
+			prev_hidden_layer 			= tf.multiply(L_1_output,tf.tanh(new_cell_state))
+			prev_cell_state 			= tf.identity(new_cell_state)
 
-	## Predicted Basket and Actual Basket Differences
-	user_new_basket_prediction 	= tf.exp(tf.matmul(user_preference,tf.transpose(product_weights)))
-	user_at_time_n1 			= tf.squeeze(tf.slice(input_data,[0,n+1,0,0],[-1,1,-1,1]),[1,3])
+		## Predicted Basket and Actual Basket Differences
+		with tf.name_scope("Predictions_{}".format(n)):	
+			user_new_basket_prediction 	= tf.sigmoid(tf.matmul(user_preference,tf.transpose(product_weights)))
+			user_at_time_n1 			= tf.squeeze(tf.slice(input_data,[0,n+1,0,0],[-1,1,-1,1]),[1,3])
 
-	## Cost function sum(Square(Y_u_i_pred - Y_u_i_actual))
-	cost 						= tf.add(cost,tf.reduce_sum(tf.square(tf.subtract(user_new_basket_prediction,user_at_time_n1))))
-	
+		## Cost function sum(Square(Y_u_i_pred - Y_u_i_actual))
+		with tf.name_scope("Cost_{}".format(n)):
+			cost 						= tf.add(cost,tf.reduce_sum(tf.abs(tf.subtract(user_new_basket_prediction,user_at_time_n1))))
+		
 
 ## Training Setup
 Alpha 			= 0.01
-decay_steps 	= 500
+decay_steps 	= 50
 decay_rate 		= 0.096
 global_step 	= tf.Variable(0,trainable = False,name = "global_step")
 learning_rate 	= tf.train.exponential_decay(Alpha,global_step,decay_steps,decay_rate,staircase = True)
-train_step 		= tf.train.AdamOptimizer(learning_rate).minimize(cost)
+optimizer 		= tf.train.AdamOptimizer(learning_rate)
+grads_and_vars 	= optimizer.compute_gradients(cost)
+train_step 		= optimizer.apply_gradients(grads_and_vars)
 
 
 ## Run Graph
@@ -83,22 +92,53 @@ init_op = tf.global_variables_initializer()
 sess.run(init_op)
 
 def print_error():
-	print(sess.run(cost,feed_dict = {input_data:in_data[0:1,:,:,:]}))
+	print(sess.run(cost,feed_dict = {input_data:in_data[0:2,:,:,:]}))
 
-def train_graph(cycles,print_cycle):		
+def aggregate_error(steps):
+	err = 0
+	for users in range(int(number_of_users / steps + steps)):
+		# print(users)
+		err += sess.run(cost,feed_dict = {input_data:in_data[users:(users+steps),:,:,:]})
+		# print(sess.run(cost,feed_dict = {input_data:in_data[users:(users+steps),:,:,:]}))
+	return err
+
+def train_graph(cycles,print_cycle,steps):		
+	# f = open('output.txt','w')
+	# f.write(str(sess.run(grads_and_vars,feed_dict = {input_data:in_data[0:1,:,:,:]})))
+	# f.close()
+
+	# print(sess.run(W_1_o))
+	# print(sess.run(W_1_i))
+	# print(sess.run(W_1_f))
+	# print(sess.run(W_1_c))
+	# print(sess.run(W_h_o))
+	# print(sess.run(W_h_i))
+	# print(sess.run(W_h_f))
+	# print(sess.run(W_h_c))
+
 	## Iterate training
 	for n in range(cycles):
-		# if(n % print_cycle):
-		# 	print(sess.run(cost,feed_dict = {input_data:in_data}))
-		sess.run(train_step,feed_dict = {input_data:in_data})
-		print(ran)
-		break
+		# print(n)
+		if((n % print_cycle) == 0):
+			print(n)
+			print(aggregate_error(1))
+			# print(sess.run(W_1_o))
+			# print(sess.run(W_1_i))
+			# print(sess.run(W_1_f))
+			# print(sess.run(W_1_c))
+			# print(sess.run(W_h_o))
+			# print(sess.run(W_h_i))
+			# print(sess.run(W_h_f))
+			# print(sess.run(W_h_c))
+		for users in range(int(number_of_users / steps + steps)):
+			sess.run(train_step,feed_dict = {input_data:in_data[users:(users+steps),:,:,:]})
+
 
 
 	writer 	= tf.summary.FileWriter('logs',sess.graph)
 	saver 	= tf.train.Saver()
 	saver.save(sess,"./model/DREAM")
-	print(sess.run(cost,feed_dict = {input_data:in_data}))
+	# print(sess.run(cost,feed_dict = {input_data:in_data}))
 
-print_error()
-# train_graph(100,100)
+# print_error()
+train_graph(30,10,1)
